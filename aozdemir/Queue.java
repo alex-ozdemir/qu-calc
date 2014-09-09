8e3542bf25525a84b871e61ca98128fb98cfdc20
Alex Ozdemir <aozdemir@hmc.edu>
2014-09-08 18:50:29 -0700

    The initial version of the project.
    Features:
    	-Some operations
    	-Text stack display
    	-Error handling
    
    Operations:
    	-Addition
    	-Subtraction
    	-Negation
    	-Multiplication
    	-Division
    	-Power
    
    Works on doubles.

---
+++ aozdemir/Queue.java
@@ -0,0 +1,63 @@
+
+package aozdemir;
+
+import java.util.LinkedList;
+
+import aozdemir.exceptions.OperandNotRecognized;
+import aozdemir.exceptions.TooFewOperands;
+
+public class Queue extends LinkedList<Double>{
+	private static final long serialVersionUID = -3735516454660342283L;
+
+	protected void operate(char op) throws TooFewOperands, OperandNotRecognized {
+		switch(op) {
+		case 'c':
+			this.clear();
+			return;
+		case '+':
+			this.checkOperands(2);
+			this.addLast(this.pollFirst() + this.pollFirst());
+			return;
+		case '-':
+			this.checkOperands(2);
+			this.addLast(this.pollFirst() - this.pollFirst());
+			return;
+		case '*':
+			this.checkOperands(2);
+			this.addLast(this.pollFirst() * this.pollFirst());
+			return;
+		case '/':
+			this.checkOperands(2);
+			this.addLast(this.pollFirst() / this.pollFirst());
+			return;
+		case '^':
+			this.checkOperands(2);
+			this.addLast(Math.pow(this.pollFirst(), this.pollFirst()));
+			return;
+		case 'n':
+			this.checkOperands(1);
+			this.addLast(-this.pollFirst());
+		default:
+			throw new OperandNotRecognized();
+		}
+	}
+	
+	private void checkOperands(int n) throws TooFewOperands {
+		if (this.size() < n) {
+			throw new TooFewOperands();
+		}
+	}
+	
+	public String toString() {
+		String result = "<< ";
+		if (this.isEmpty()) {
+			result += "none ";
+		} else {
+			for (Double num : this) {
+				result += num.toString() + " ";
+			}
+		}
+		result += "<<";
+		return result;
+	}
+}
---
+++ aozdemir/exceptions/TooFewOperands.java
@@ -0,0 +1,6 @@
+package aozdemir.exceptions;
+
+public class TooFewOperands extends Exception {
+	private static final long serialVersionUID = -8455156330171917120L;
+
+}
